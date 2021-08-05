0560314ae5f618cc8dd5a1927b118cd1b2850dfb
YanaHorbachova <maya69877@gmail.com>
2021-08-05 14:14:27 +0300

    доделан hero

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Section from '../Section';
+import BgText from '../../img/bgherotext.png'
 import Mi from '../../img/Mi.jpg';
 import style from './AboutMe.module.css';
 
@@ -13,7 +14,13 @@ const AboutMe = () => {
                     />
                     <p className={style.text}>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. 
-                    </p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. 
+                   </p>
+                   <img
+                        className={style.ImageBg}
+                        src={BgText}
+                        alt= 'Фон текста'
+                    />
 
             </Section>
       );
