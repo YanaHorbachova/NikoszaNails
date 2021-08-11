49965cc0d6733ad7a895131c6f8dbe4eb03a56d9
YanaHorbachova <maya69877@gmail.com>
2021-08-11 16:18:02 +0300

    дописана ссылка в кнопке

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,9 +3,12 @@ import styles from './Button.module.css';
 const Button = ({text}) => {
     return (
       <button type="button" className={styles.button}>
-        {text}
+       <a href="https://booksy.com/pl-pl/80275_nikosza-nails_paznokcie_13750_wroclaw">
+         {text}</a> 
       </button>
     );
   }
 
 export default Button;
+
+// Чтобы переход на запись окрывался в новой вкладке target="_blank"
